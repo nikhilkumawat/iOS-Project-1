a0bc3d0efa5d4a68fa23ef9bb33eeae5fc0e66e0
SCL IT <sclit@Shrees-Mac-mini.local>
2018-08-08 15:18:50 +0530

    First Commit to push GitHub

---
+++ project2.1/HomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  HomeVC.swift
+//  project2.1
+//
+//  Created by SCL IT on 08/08/18.
+//  Copyright © 2018 Nikhil. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+
+}
